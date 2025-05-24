390771d90e788dad14cf36e6d2f05f11cb409091
Luca Seimandi <lseimandi@colsecor.com.ar>
2025-05-24 18:10:47 -0300

    modified code for part 3.19

--- part2/phonebook/src/services/persons.jsx
+++ part2/phonebook/src/services/persons.jsx
@@ -1,29 +1,38 @@
 import axios from "axios";
-// const baseUrl = "http://localhost:3001/api/persons"; // Small adjustment to the base url for exercise for part 3.9
+const baseUrl = "http://localhost:3001/api/persons"; // Small adjustment to the base url for exercise for part 3.9
 // const baseUrl = "/api/persons"; // Changing base url to relative for deployment in exercise 3.10
-const baseUrl =
-  "https://fullstack-open-exercises-part3-w00b.onrender.com/api/persons"; // New base url for deployment in Render for exercise 3.11 (testing locally)
+// const baseUrl = "https://fullstack-open-exercises-part3-w00b.onrender.com/api/persons"; // New base url for deployment in Render for exercise 3.11 (testing locally)
 const getAllPersons = () => {
   return axios.get(baseUrl).then((response) => response.data);
 };
 
 // New method for connecting to the backend for part 3.9
 const getPersonById = (id) => {
-  return axios.get(`${baseUrl}/${id}`).then((response) => response.data);
+  return axios
+    .get(`${baseUrl}/${id}`)
+    .then((response) => response.data)
+    .catch((error) => console.log(error.data.response.data.error));
 };
 
 const createPerson = (newPersonObject) => {
-  return axios.post(baseUrl, newPersonObject).then((response) => response.data);
+  return axios
+    .post(baseUrl, newPersonObject)
+    .then((response) => response.data)
+    .catch((error) => console.log(error.data.response.data.error));
 };
 
 const deletePerson = (id) => {
-  return axios.delete(`${baseUrl}/${id}`).then((response) => response.data);
+  return axios
+    .delete(`${baseUrl}/${id}`)
+    .then((response) => response.data)
+    .catch((error) => console.log(error.data.response.data.error));
 };
 
 const updatePerson = (id, newPersonObject) => {
   return axios
     .put(`${baseUrl}/${id}`, newPersonObject)
-    .then((response) => response.data);
+    .then((response) => response.data)
+    .catch((error) => console.log(error.data.response.data.error));
 };
 
 export default { getAllPersons, createPerson, deletePerson, updatePerson };
